a567fc08d91901a2e0ef3f3da5a19ad506ea25c9
Kubo1805 <kunallulla806@gmail.com>
2026-02-08 04:19:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -60,16 +60,29 @@ const CONFIG = {
     message: "Now come get your gift, a big warm hug and a huge kiss!",
     emojis: "🎁💖🤗💝💋❤️💕",  // These will bounce around
     loveLetter: `
-My Gracie,
 
-My babyyy, my love, my swweetheart, my angel, my world, my princess, my soulmate, my forever and ever.
-First of all I want to sayy, Happpyyyy Valentines my loveeee 🩷🩷🩷
-And I just want to tell you... holy aaaaa not me already crying aaa,
-I just want to tell u my babyy how much you mean to me, how much these 9 months of knowing each other
-and us 6 months of dating means to me.
-And its so hard to put it in words how much I love you and how much my time with you means to me my love My baby your existence in my life and you being mine has made me realize what true happiness reallyy means I get to wake up everyday with a smile on my face cause as soon as I wake up, I think of you my love and you are always right there, your adorable face, your adorable snores, sleeping with such an adorable and peaceful face And being able to go to sleep everyday while looking at your gorgeous and adorable face makes me have the best sleep ever, and you makes me the happiest I can be while going to sleep And I love my entire day alll because of you my lovee, the missing you when your not there or I am at work and then being able to come home and spend time with you my love is a feeling I loveeeee soo much I love every single second that we spend together and every single second that we spend with each other makes me fall even more in love with you Because I get to understand how amazing you are and how adorable you are, and howwww hot and sexyyyy you are cutieee I assume you saw the pictures of you on the previous page, i was scrolling through your pics and it was soooo hard to choose six pictures and i was getting the most insane cute aggression in human history You are genuinely the cutest thin in human history my lovee I want to cherish you, i want to love you, i want to see that adorable and precious smile every single day and I will do anything to make my baby the happiest human alive in human history I LOVE YOUUU SOOOO MUCH MY GRACIEEEEE I LOVEEE YOU TO PLUTO AND BACKK AND I LOVE U TO ALL THE STARS IN THE UNIVERSE AND BACK MY LOVE
-Forever yours,
-Your Kunal 💖
+      My Gracie,
+
+          My babyyy, my love, my swweetheart, my angel, my world, my princess, my soulmate, my forever and ever
+          First of all I want to sayy, Happpyyyy Valentines my loveeee 🩷🩷🩷
+          And I just want to tell you . holy aaaaa not me already cryying aaa,
+          I just want to tell u my babyy how much you mean to me, how much these 9 months of knowing each other and us 6 months of dating means to me
+          And its so hard to put it in words how much I love you and how much my time with you means to me my love
+          My baby your existence in my life and you being mine has made me realize what true happiness reallyy means
+          I get to wake up everyday with a smile on my face cause as soon as I wake up, I think of you my love and you are always right there, your adorable face, your adorable snores, sleeping with such an adorable and peaceful face           
+          And being able to go to sleep everyday while looking at your gorgeous and adorable face makes me have the best sleep ever, and you makes me the happiest I can be while going to sleep
+          And I love my entire day alll because of you my lovee, the missing you when your not there or I am at work and then being able to come home and spend time with you my love is a feeling I loveeeee soo much
+          I love every single second that we spend together and every single second that we spend with each other makes me fall even more in love with you
+          Because I get to understand how amazing you are and how adorable you are, and howwww hot and sexyyyy you are cutieee
+          I assume you saw the pictures of you on the previous page, i was scrolling through your pics and it was soooo hard to choose six pictures and i was getting the most insane cute aggression in human history 
+          You are genuinely the cutest thin in human history my lovee
+          I want to cherish you, i want to love you, i want to see that adorable and precious smile every single day and I will do anything to make my baby the happiest human alive in human history          
+          I LOVE YOUUU SOOOO MUCH MY GRACIEEEEE
+          I LOVEEE YOU TO PLUTO AND BACKK
+          AND I LOVE U TO ALL THE STARS IN THE UNIVERSE AND BACK MY LOVE          
+                    
+        Forever yours,
+        Your Kunal 💖
 `
 },
 
