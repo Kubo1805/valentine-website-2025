7aa1e4b761d5a73a60f4e91c5feb2a58df168893
Kubo1805 <kunallulla806@gmail.com>
2026-02-09 01:43:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -97,7 +97,7 @@ I LOVE YOU SOOOO MUCH, I LOVE YOU TO PLUTO AND BACK AND I LOVE YOU TO ALLLL THE 
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dmmawtk9o/video/upload/v1770581528/Laufey_-_From_The_Start_Official_Music_Video_fxcfmb.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
